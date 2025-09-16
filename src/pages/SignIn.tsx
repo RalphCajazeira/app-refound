cd80f303d9f9d5d2f9536b376e027562679b3677
Ralph Cajazeira <ralphmtk@gmail.com>
2025-09-16 18:04:50 -0300

    Link para SignUp

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,3 +1,39 @@
+import { useState } from "react";
+import { Button } from "../components/Button";
+import { Input } from "../components/Input";
+
 export function SignIn() {
-  return <h1>SignIn</h1>;
-} 
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  function onSubmit(e: React.FormEvent) {
+    e.preventDefault();
+
+    console.log(email, password);
+  }
+
+  return (
+    <form onSubmit={onSubmit} className="flex flex-col gap-4">
+      <Input
+        required
+        legend="E-mail"
+        type="email"
+        placeholder="seu@email.com"
+        onChange={(e) => setEmail(e.target.value)}
+      />
+
+      <Input
+        required
+        legend="Senha"
+        type="password"
+        placeholder="Digite sua senha"
+        onChange={(e) => setPassword(e.target.value)}
+      />
+
+      <Button type="submit" isLoading={isLoading}>
+        Entrar
+      </Button>
+    </form>
+  );
+}
