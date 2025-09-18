aa027f733a584dfd540e11aa6b71a9ff6f854d06
Ralph Cajazeira <ralphmtk@gmail.com>
2025-09-18 16:10:57 -0300

    Button Enviar

--- src/pages/Refound.tsx
+++ src/pages/Refound.tsx
@@ -1,8 +1,10 @@
 import { useState } from "react";
 
 import { CATEGORIES, CATEGORIES_KEYS } from "../utils/categories";
+
 import { Input } from "../components/Input";
 import { Select } from "../components/Select";
+import { Upload } from "../components/Upload";
 
 export function Refound() {
   const [category, setCategory] = useState("");
@@ -35,6 +37,8 @@ export function Refound() {
 
         <Input legend="Valor" required />
       </div>
+
+      <Upload filename="Ralph.png" />
     </form>
   );
 }
