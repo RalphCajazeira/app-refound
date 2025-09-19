7096e07aa90b3d343351361eaade506f466baf21
Ralph Cajazeira <ralphmtk@gmail.com>
2025-09-19 16:13:29 -0300

    formatCurrency

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -41,7 +41,11 @@ export function Dashboard() {
         </Button>
       </form>
 
-      <div>
+      <div className="mt-6 flex flex-col gap-4 max-h-[342px] overflow-y-scroll">
+        <RefoundItem data={REFOUND_EXAMPLE} />
+        <RefoundItem data={REFOUND_EXAMPLE} />
+        <RefoundItem data={REFOUND_EXAMPLE} />
+        <RefoundItem data={REFOUND_EXAMPLE} />
         <RefoundItem data={REFOUND_EXAMPLE} />
       </div>
     </div>
