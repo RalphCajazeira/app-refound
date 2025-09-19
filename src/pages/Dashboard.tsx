c5ebf1c360d0c8605e861c27c264a7e857105528
Ralph Cajazeira <ralphmtk@gmail.com>
2025-09-19 16:06:29 -0300

    Customizando Scroll

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -4,11 +4,21 @@ import searchSvg from "../assets/search.svg";
 
 import { Input } from "../components/Input";
 import { Button } from "../components/Button";
+import { RefoundItem } from "../components/RefoundItem";
+import { CATEGORIES } from "../utils/categories";
+
+const REFOUND_EXAMPLE = {
+  id: "123",
+  name: "Ralph",
+  category: "Transporte",
+  amount: "12,50",
+  categoryImg: CATEGORIES["transport"].icon,
+};
 
 export function Dashboard() {
   const [name, setName] = useState("");
 
-  function fatchRefunds(e: React.FormEvent) {
+  function fetchRefunds(e: React.FormEvent) {
     e.preventDefault();
     console.log(name);
   }
@@ -18,7 +28,7 @@ export function Dashboard() {
       <h1 className="text-gray-100 font-bold text-xl flex-1">Solicitações</h1>
 
       <form
-        onSubmit={fatchRefunds}
+        onSubmit={fetchRefunds}
         className="flex items-center justify-between pb-6 border-b-[1px] border-b-gray-400 md:flex-row gap-2 mt-6"
       >
         <Input
@@ -30,6 +40,10 @@ export function Dashboard() {
           <img src={searchSvg} alt="Ícone de pesquisa" className="w-5" />
         </Button>
       </form>
+
+      <div>
+        <RefoundItem data={REFOUND_EXAMPLE} />
+      </div>
     </div>
   );
 }
