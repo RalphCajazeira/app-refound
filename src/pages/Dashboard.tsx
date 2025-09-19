63e8deeaa1cf083275e41116a2530b64c2839fe9
Ralph Cajazeira <ralphmtk@gmail.com>
2025-09-19 15:48:18 -0300

    Componente RefundItem

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,3 +1,35 @@
+import { useState } from "react";
+
+import searchSvg from "../assets/search.svg";
+
+import { Input } from "../components/Input";
+import { Button } from "../components/Button";
+
 export function Dashboard() {
-  return <h1>Dashboard</h1>;
+  const [name, setName] = useState("");
+
+  function fatchRefunds(e: React.FormEvent) {
+    e.preventDefault();
+    console.log(name);
+  }
+
+  return (
+    <div className="bg-gray-500 rounded-xl p-10 md:min-w-[768px]">
+      <h1 className="text-gray-100 font-bold text-xl flex-1">Solicitações</h1>
+
+      <form
+        onSubmit={fatchRefunds}
+        className="flex items-center justify-between pb-6 border-b-[1px] border-b-gray-400 md:flex-row gap-2 mt-6"
+      >
+        <Input
+          placeholder="Pesquisar"
+          onChange={(e) => setName(e.target.value)}
+        />
+
+        <Button type="submit" variant="icon">
+          <img src={searchSvg} alt="Ícone de pesquisa" className="w-5" />
+        </Button>
+      </form>
+    </div>
+  );
 }
